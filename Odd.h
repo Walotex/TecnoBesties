7967c4f9c77ecc609102ddbceeae6a32cdc37547
danycuso <danyvartiles1@gmail.com>
2022-04-03 17:02:10 -0500

    Odd_commit_1

---
+++ Odd.h
@@ -0,0 +1,26 @@
+/**********************************************
+ *  Odd Function.h                            *
+ *  Daniel Valdes Artiles                     *
+ *  Algoritmos 1B                             *
+ **********************************************/
+
+#include <stdbool.h>
+
+int Odd ( int arr[], int len ){
+    
+    bool counterOdd = 0;
+
+    for (int i = 0;  i > len; i++){
+
+        if (arr[i] % 2 == 0){
+
+            counterOdd++;
+            return counterOdd;
+
+        };
+    
+    };
+return counterOdd;
+
+
+};
