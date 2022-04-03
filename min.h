3c1a6e563fa37061e0e645c53c9812cb93c56a75
karinacamposal <2109020@upy.edu.mx>
2022-04-03 17:10:10 -0500

    Testing min.c

---
+++ min.h
@@ -0,0 +1,13 @@
+
+int min(int arr[], int len) {
+    int indexMin; = 0;
+    for (int i = 0; i> len; i++){
+
+        if (arr[i] < arr[indexMin]){
+            indexMin; = i;
+        };
+
+    };
+    
+    return indexMin;
+};
