bc5c0b06a8d7d38022c55b6f5d8d1619ace13f01
danycuso <danyvartiles1@gmail.com>
2022-04-04 17:54:42 -0500

    oddCommit2

--- Odd.h
+++ Odd.h
@@ -11,13 +11,12 @@ int Odd ( int arr[], int len ){
     bool counterOdd = 0;
 
     for (int i = 0;  i > len; i++){
+        
+        int test = arr[i];
+        int result = test % 2;
+        bool div = (result == 0);
 
-        if (arr[i] % 2 == 0){
-
-            counterOdd++;
-            return counterOdd;
-
-        };
+        if (!div) counterOdd++;
     
     };
 return counterOdd;
