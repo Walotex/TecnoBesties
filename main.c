60a097dcf82b05f817b6ddd581271661da2deb59
walote <1909123@upy.edu.mx>
2022-04-04 20:38:34 -0500

    main arrays arreglados

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 #include "min.h"
 #include "find.h"
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int main (){
+    int array[] = {3, 1 ,10, 14};
+    printf("Array values: {%d, %d, %d, %d}\n", array [0], array [1], array [2], array [3]);
+    printf("Max values: %d\n", max(array, 4));
+    printf("Min values: %d\n", min(array, 4));
+    printf("Odd values: {"};
+    Odd(array, 4);
+    printf("}\n");
+
     return 0;
 }
